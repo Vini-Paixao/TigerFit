0180ada6cb0dc8142285eb906524ba176ae34136
Marcus Vinicius <marcuspaixao0@gmail.com>
2023-04-21 20:54:36 -0300

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,85 @@
+(function() {
+
+  'use strict';
+
+  // preloader
+  $(window).on('load', function() {
+    $('.loader').fadeOut('slow');
+  });
+
+  // smooth scroll
+  $("a").on("click", function(event) {
+
+      if (this.hash !== "") {
+          event.preventDefault();
+
+          var hash = this.hash;
+
+          $("html, body").animate({
+
+              scrollTop: $(hash).offset().top - 50
+
+          }, 850);
+
+      }
+
+  });
+
+  // magnific popup
+  $('.gallery').each(function() { // the containers for all your galleries
+      $(this).magnificPopup({
+          delegate: '.popup-image', // the selector for portfolio item
+          type: 'image',
+          gallery: {
+              enabled: true
+          },
+      });
+  });
+
+  // swiper slider
+  $(document).ready(function () {
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".next-slide",
+        prevEl: ".prev-slide"
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 1,
+        },
+        780: {
+          slidesPerView: 1,
+        }
+      }
+    });
+  });
+
+  // hide navbar on click
+  $('.navbar-nav>li>a').on('click', function(){
+      $('.navbar-collapse').collapse('hide');
+  });
+
+  // navbar on scroll
+  $(window).on("scroll", function() {
+
+      var onScroll = $(this).scrollTop();
+
+      if( onScroll > 50) {
+          $(".navbar").addClass("navbar-fixed");
+      }
+      else {
+          $(".navbar").removeClass("navbar-fixed");
+      }
+
+  });
+
+})();
